aec149cd54b903240bb41e862f889981be0b7b0b
Chris Roberts <chrisroberts.code@gmail.com>
2012-04-17 16:50:12 -0700

    Bump version. Note bug fix in change log.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 CHANGELOG
 =========
 
+v0.0.3
+------
+* Removes extra comma from nginx recipe causing issues with non-1.9.3 rubies
+
 v0.0.2
 ------
 * Adds runit init_type support
--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email 'chrisroberts.code@gmail.com'
 license 'Apache 2.0'
 description 'Installs and configures gitpaste pastebin app'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.0.2'
+version '0.0.3'
 
 %w(python gunicorn nginx bluepill runit).each do |dep_book|
   depends dep_book
