7edf7467fe3b68c9ed474bc7d4611da5f8188a6c
Chris Roberts <chrisroberts.code@gmail.com>
2012-04-17 10:58:28 -0700

    Add runit dependency. Bump version

--- metadata.rb
+++ metadata.rb
@@ -3,9 +3,9 @@ maintainer_email 'chrisroberts.code@gmail.com'
 license 'Apache 2.0'
 description 'Installs and configures gitpaste pastebin app'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.0.1'
+version '0.0.2'
 
-%w(python gunicorn nginx bluepill).each do |dep_book|
+%w(python gunicorn nginx bluepill runit).each do |dep_book|
   depends dep_book
 end
 
